00678d0ca80e3a8e85ec0e31b56ea68462bba65b
Michael Bianchi <michaeldbianchi@gmail.com>
2020-08-19 20:42:39 -0500

    [MB] Add stocks

--- site/src/templates/stock.js
+++ site/src/templates/stock.js
@@ -2,21 +2,22 @@ import React from "react"
 import Highcharts from 'highcharts/highstock'
 import HighchartsReact from 'highcharts-react-official'
 import { graphql } from 'gatsby'
+import Title from "@lekoarts/gatsby-theme-minimal-blog/src/components/title"
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
-export default function Stock({ data }) {
+export default function stock({ data }) {
   const stockData = data.stocksJson
   const hypotheticalGrowth = stockData.hypotheticalGrowth
   const hypotheticalGrowthOptions = {
     title: {
-      text: stockData.name
+      text: "10 Year Hypothetical Growth"
     },
     series: hypotheticalGrowth.series
   }
   return (
     <Layout>
-      <div>Hi from stock template</div>
       <div>
-        <h1>name: {stockData.name}</h1>
+        <Title text={stockData.name} />
+        <h2>Returns</h2>
         <div>
           <HighchartsReact
             highcharts={Highcharts}
